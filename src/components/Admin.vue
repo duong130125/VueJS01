4922249827931ef60587de8ba425d829f9763ce5
Duong <nguyendinhduong2005yt@gmail.com>
2024-09-18 23:37:43 +0700

    vue01

---
+++ src/components/Admin.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="admin-layout">
+      <header class="header">Header</header>
+      <div class="content">
+        <nav class="menu">Menu</nav>
+        <main class="main">Main</main>
+      </div>
+      <footer class="footer">Footer</footer>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AdminIndex'
+  }
+  </script>
+  
+  <style scoped>
+  .admin-layout {
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+  }
+  
+  .header, .footer {
+    height: 40px;
+    background-color: #cac5c5;
+    padding: 10px;
+    text-align: center;
+  }
+  
+  .content {
+    display: flex;
+    flex: 1;
+    text-align: center;
+
+  }
+  
+  .menu {
+    background-color: #003366;
+    color: white;
+    width: 200px;
+    padding: 20px;
+    text-align: center;
+
+  }
+  
+  .main {
+    flex: 1;
+    padding: 20px;
+  }
+  </style>
